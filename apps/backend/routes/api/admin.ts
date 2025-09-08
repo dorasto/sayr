b44ee29177ff146e8329bda396bd864a72c58cf7
trent-001 <trent@doras.to>
2025-09-09 00:45:42 +1000

    custom role check

--- apps/backend/routes/api/admin.ts
+++ apps/backend/routes/api/admin.ts
@@ -1,6 +1,6 @@
-import { auth } from "@repo/auth";
-// import { auth as authType, db } from "@repo/database";
-// import { eq } from "drizzle-orm";
+import type { auth } from "@repo/auth";
+import { auth as authType, db } from "@repo/database";
+import { and, eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { broadcast, findClientByWsId } from "../ws";
 
@@ -14,21 +14,27 @@ export const apiRouteAdmin = new Hono<{
 apiRouteAdmin.post("/update-org", async (c) => {
 	try {
 		const { org_id, wsClientId, data } = await c.req.json();
-		const startNew = Date.now();
+		const session = c.get("session");
+		const start = Date.now();
+		const role = await db
+			.select()
+			.from(authType.member)
+			.where(and(eq(authType.member.userId, session?.userId || ""), eq(authType.member.organizationId, org_id)));
+		console.log("🚀 ~ roles:", role[0]?.role);
+		console.log("hasPermission fetch took", Date.now() - start, "ms");
 
-		const result = await auth.api.updateOrganization({
-			body: {
-				data,
-				organizationId: org_id,
-			},
-			headers: c.req.raw.headers,
-		});
+		const startNew = Date.now();
+		const result = await db
+			.update(authType.organization)
+			.set({ ...data, updatedAt: new Date() })
+			.where(eq(authType.organization.id, org_id))
+			.returning();
 		console.log("updateOrganization fetch took", Date.now() - startNew, "ms");
 
-		if (result) {
+		if (result[0]) {
 			const found = findClientByWsId(wsClientId);
-			broadcast(org_id, "admin", { type: "UPDATE_ORG", data: result }, found?.socket);
-			return c.json({ success: true, data: result });
+			broadcast(org_id, "admin", { type: "UPDATE_ORG", data: result[0] }, found?.socket);
+			return c.json({ success: true, data: result[0] });
 		}
 		return c.json({ error: "UNAUTHORIZED" }, 401);
 		// biome-ignore lint/suspicious/noExplicitAny: <has to be any>
@@ -36,6 +42,7 @@ apiRouteAdmin.post("/update-org", async (c) => {
 		console.log("🚀 ~ error:", error);
 		return c.json(
 			{
+				path: c.req.path,
 				error: error.status,
 			},
 			error.statusCode
