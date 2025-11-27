a0453d5905aa72a70d411eaf1476cc97dc3938d3
Tommy Lundy <tommerty@doras.to>
2025-11-27 20:30:14 +0000

    Styling on task heading

--- apps/web/src/app/components/layout/admin/admin-navigation/TasksPage.tsx
+++ apps/web/src/app/components/layout/admin/admin-navigation/TasksPage.tsx
@@ -1,16 +1,25 @@
 "use client";
 import type { schema } from "@repo/database";
+import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/components/avatar";
 import {
 	Breadcrumb,
 	BreadcrumbItem,
 	BreadcrumbLink,
 	BreadcrumbList,
+	BreadcrumbPage,
 	BreadcrumbSeparator,
 } from "@repo/ui/components/breadcrumb";
 import { Button } from "@repo/ui/components/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@repo/ui/components/dropdown-menu";
 import { Skeleton } from "@repo/ui/components/skeleton";
 import { useStateManagement } from "@repo/ui/hooks/useStateManagement.ts";
-import { IconPlus, IconSettings, IconSlash } from "@tabler/icons-react";
+import { IconPlus, IconSettings, IconSlash, IconUsers } from "@tabler/icons-react";
+import { ChevronDownIcon } from "lucide-react";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
@@ -25,25 +34,52 @@ export default function TasksPage() {
 	const [openNew, setOpenNew] = useState(false);
 	if (!organization || !tasks) {
 		return (
-			<Skeleton className="flex items-center gap-2 shrink-0 rounded bg-accent border px-3 py-0.5 h-9 shadow-xs w-full justify-start" />
+			<Skeleton className="flex items-center gap-2 shrink-0 rounded px-3 py-0.5 h-9 shadow-xs w-full justify-start bg-sidebar" />
 		);
 	}
 	return (
 		<div className="flex items-center gap-3 w-full">
-			<div className="flex items-center gap-2 shrink-0 rounded bg-accent border px-3 py-0.5 h-9 shadow-xs w-fit justify-start">
-				<Breadcrumb>
-					<BreadcrumbList className="">
-						<BreadcrumbItem>
-							<BreadcrumbLink href={`/admin/${organization?.id}`}>{organization?.name}</BreadcrumbLink>
+			<div className="flex items-center gap-2 shrink-0 rounded-lg bg-accent h-9 shadow-xs w-fit justify-start overflow-hidden">
+				<Breadcrumb className="h-full">
+					<BreadcrumbList className="h-full gap-0">
+						<BreadcrumbItem className="h-full">
+							<BreadcrumbLink
+								href={`/admin/${organization?.id}`}
+								className="flex items-center gap-1 px-2 py-0.5 h-full"
+							>
+								<Avatar className="h-3.5 w-3.5">
+									<AvatarImage src={organization.logo || ""} alt={organization.name} className="" />
+									<AvatarFallback className="rounded-md uppercase text-xs">
+										<IconUsers className="h-3.5 w-3.5" />
+									</AvatarFallback>
+								</Avatar>
+								{organization?.name}
+							</BreadcrumbLink>
 						</BreadcrumbItem>
-						<BreadcrumbSeparator>
-							<IconSlash />
+						<BreadcrumbSeparator className="h-full flex items-center">
+							<IconSlash className="h-full" />
 						</BreadcrumbSeparator>
-						<BreadcrumbItem>
-							<Button variant={"ghost"} size={"sm"} className="h-auto w-auto p-0 gap-1 items-center">
-								<IconSettings className="!size-4" /> Tasks
-							</Button>
+						<BreadcrumbItem className="h-full">
+							<DropdownMenu>
+								<DropdownMenuTrigger
+									asChild
+									// className="flex items-center gap-1 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5"
+								>
+									<BreadcrumbPage className="flex items-center gap-1 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5 cursor-pointer hover:bg-border/80 h-full px-2 py-0.5">
+										All tasks
+										<ChevronDownIcon />
+									</BreadcrumbPage>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent align="start">
+									<DropdownMenuItem>Documentation</DropdownMenuItem>
+									<DropdownMenuItem>Themes</DropdownMenuItem>
+									<DropdownMenuItem>GitHub</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenu>
 						</BreadcrumbItem>
+						{/* <BreadcrumbItem>
+							<BreadcrumbLink href={`/admin/${organization?.id}/tasks`}>All tasks</BreadcrumbLink>
+						</BreadcrumbItem> */}
 					</BreadcrumbList>
 				</Breadcrumb>
 			</div>
@@ -51,7 +87,7 @@ export default function TasksPage() {
 			{/* Task Filters */}
 			{/* <TaskFilterDropdown tasks={tasks} labels={labels} availableUsers={availableUsers} /> */}
 
-			<Button variant={"accent"} size={"sm"} className="h-9" onClick={() => setOpenNew(true)}>
+			<Button variant={"accent"} size={"sm"} className="h-9 rounded-lg border-0" onClick={() => setOpenNew(true)}>
 				<IconPlus />
 				<span className="text-inherit">New task</span>
 			</Button>
