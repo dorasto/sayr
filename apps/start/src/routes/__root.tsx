ff2f937961332f412acde9e9e162dad6e715e5d2
Tommy Lundy <tommerty@doras.to>
2025-12-06 20:12:56 +0000

    Add suppressHydrationWarning to RootDocument HTML tag

--- apps/start/src/routes/__root.tsx
+++ apps/start/src/routes/__root.tsx
@@ -38,7 +38,7 @@ export const Route = createRootRouteWithContext<{
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<HeadContent />
 				<SidebarScript />
