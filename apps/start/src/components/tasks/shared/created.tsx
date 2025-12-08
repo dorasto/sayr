db1ceb591ca296bf9c5b9a31def65b367f84792d
Tommy Lundy <tommerty@doras.to>
2025-12-08 13:38:52 +0000

    Update created.tsx

--- apps/start/src/components/tasks/shared/created.tsx
+++ apps/start/src/components/tasks/shared/created.tsx
@@ -7,15 +7,21 @@ import { formatDateTime } from "@repo/util";
 interface GlobalTaskCreatedAtProps {
 	task: schema.TaskWithLabels;
 }
-export default function GlobalTaskCreatedAt({ task }: GlobalTaskCreatedAtProps) {
+export default function GlobalTaskCreatedAt({
+	task,
+}: GlobalTaskCreatedAtProps) {
 	const createdAt = new Date(task.createdAt as Date);
 	const updatedAt = new Date(task.updatedAt as Date);
 
 	return (
 		<div className="flex flex-col gap-3">
-			<Label variant={"subheading"}>Created at {formatDateTime(createdAt)}</Label>
+			<Label variant={"subheading"}>
+				Created at {formatDateTime(createdAt)}
+			</Label>
 			{createdAt.getTime() < updatedAt.getTime() && (
-				<Label variant={"description"}>Last updated at {formatDateTime(updatedAt)}</Label>
+				<Label variant={"description"}>
+					Last updated at {formatDateTime(updatedAt)}
+				</Label>
 			)}
 		</div>
 	);
