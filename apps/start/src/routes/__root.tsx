8cf85e47c8d0d9ab0e92e8562175f71a71cc5c7e
trent-001 <trent@doras.to>
2025-12-23 17:52:58 +1100

    fixed manifest file

--- apps/start/src/routes/__root.tsx
+++ apps/start/src/routes/__root.tsx
@@ -34,6 +34,10 @@ export const Route = createRootRouteWithContext<{
 				rel: "stylesheet",
 				href: appCss,
 			},
+			{
+				rel: "manifest",
+				href: "/manifest.json",
+			},
 		],
 	}),
 	notFoundComponent: NotFound,
