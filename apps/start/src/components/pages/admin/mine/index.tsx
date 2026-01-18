30c7ab8219b40d65f0f1865daa685a16fe14ff5c
Tommy Lundy <tommerty@doras.to>
2026-01-18 15:14:28 +0000

    Update index.tsx

--- apps/start/src/components/pages/admin/mine/index.tsx
+++ apps/start/src/components/pages/admin/mine/index.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { useMyTasks } from "@/contexts/ContextMine";
 import { MyTasksList } from "./task-list";
 import { MyTaskDetail } from "./task-detail";
+import { useIsMobile } from "@repo/ui/hooks/use-mobile.tsx";
 
 export default function MyTasksPage() {
   const { tasks, setTasks, labels, categories } = useMyTasks();
@@ -23,49 +24,66 @@ export default function MyTasksPage() {
         .map((t) => [t.organization!.id, t.organization!]),
     ).values(),
   );
-
+  const isMobile = useIsMobile();
   return (
     <div className="relative flex flex-col h-full max-h-full">
-      <ResizablePanelGroup direction="horizontal" className="h-full">
-        {/* Left panel - Task list with filters */}
-        <ResizablePanel defaultSize={25} minSize={10} maxSize={30} className="">
-          <div className="flex-1 overflow-hidden h-full flex flex-col">
-            <MyTasksList
-              tasks={tasks}
-              setTasks={setTasks}
-              selectedTask={selectedTask}
-              setSelectedTask={setSelectedTask}
-              organizations={organizations}
-              labels={labels}
-              categories={categories}
-            />
-          </div>
-        </ResizablePanel>
-
-        <ResizableHandle />
-
-        {/* Right panel - Task detail */}
-        <ResizablePanel defaultSize={75}>
-          <div
-            className={cn(
-              "flex-1 overflow-y-auto h-full flex flex-col relative",
-            )}
+      {isMobile ? (
+        <MyTasksList
+          tasks={tasks}
+          setTasks={setTasks}
+          selectedTask={selectedTask}
+          setSelectedTask={setSelectedTask}
+          organizations={organizations}
+          labels={labels}
+          categories={categories}
+        />
+      ) : (
+        <ResizablePanelGroup direction="horizontal" className="h-full">
+          {/* Left panel - Task list with filters */}
+          <ResizablePanel
+            defaultSize={25}
+            minSize={10}
+            maxSize={30}
+            className=""
           >
-            {selectedTask ? (
-              <MyTaskDetail
-                task={selectedTask}
+            <div className="flex-1 overflow-hidden h-full flex flex-col">
+              <MyTasksList
                 tasks={tasks}
                 setTasks={setTasks}
+                selectedTask={selectedTask}
                 setSelectedTask={setSelectedTask}
+                organizations={organizations}
                 labels={labels}
                 categories={categories}
               />
-            ) : (
-              <div className="flex items-center justify-center h-full text-muted-foreground"></div>
-            )}
-          </div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+            </div>
+          </ResizablePanel>
+
+          <ResizableHandle />
+
+          {/* Right panel - Task detail */}
+          <ResizablePanel defaultSize={75}>
+            <div
+              className={cn(
+                "flex-1 overflow-y-auto h-full flex flex-col relative",
+              )}
+            >
+              {selectedTask ? (
+                <MyTaskDetail
+                  task={selectedTask}
+                  tasks={tasks}
+                  setTasks={setTasks}
+                  setSelectedTask={setSelectedTask}
+                  labels={labels}
+                  categories={categories}
+                />
+              ) : (
+                <div className="flex items-center justify-center h-full text-muted-foreground"></div>
+              )}
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      )}
     </div>
   );
 }
