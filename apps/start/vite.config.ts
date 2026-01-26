509d6e5a618f3de395c2905c1c5e588982bed96b
Tommy Lundy <tommerty@doras.to>
2026-01-26 16:05:36 +0000

    Update vite.config.ts

--- apps/start/vite.config.ts
+++ apps/start/vite.config.ts
@@ -7,35 +7,42 @@ import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
-	define: {
-		"import.meta.env.VITE_APP_ENV": JSON.stringify(process.env.APP_ENV ?? "development"),
-		"import.meta.env.VITE_SAYR_CLOUD": JSON.stringify(process.env.SAYR_CLOUD ?? "false"),
-	},
-	plugins: [
-		devtools(),
-		nitro({
-			externals: {
-				inline: ["@tabler/icons-react", "lucide-react"],
-			},
-			routeRules: {
-				"/api/auth/**": {}, // local auth
-				"/api/image-preview/**": {},
-				// ✅ proxy ONLY backend-api
-				"/backend-api/**": {
-					proxy: {
-						to: "http://localhost:5468/api/**",
-					},
-				},
-			},
-		}),
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-		tailwindcss(),
-		tanstackStart(),
-		viteReact(),
-	],
+  build: {
+    minify: false,
+  },
+  define: {
+    "import.meta.env.VITE_APP_ENV": JSON.stringify(
+      process.env.APP_ENV ?? "development",
+    ),
+    "import.meta.env.VITE_SAYR_CLOUD": JSON.stringify(
+      process.env.SAYR_CLOUD ?? "false",
+    ),
+  },
+  plugins: [
+    devtools(),
+    nitro({
+      externals: {
+        inline: ["@tabler/icons-react", "lucide-react"],
+      },
+      routeRules: {
+        "/api/auth/**": {}, // local auth
+        "/api/image-preview/**": {},
+        // ✅ proxy ONLY backend-api
+        "/backend-api/**": {
+          proxy: {
+            to: "http://localhost:5468/api/**",
+          },
+        },
+      },
+    }),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
 });
 
 export default config;
