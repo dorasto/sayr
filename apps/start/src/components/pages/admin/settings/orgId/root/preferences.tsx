221707a2485d70fa9561b539e940064e20589c33
Tommy Lundy <tommerty@doras.to>
2026-02-23 19:10:58 +0000

    Move org settings to root and add preferences in progress. ref 12

---
+++ apps/start/src/components/pages/admin/settings/orgId/root/preferences.tsx
@@ -0,0 +1,79 @@
+import {
+  Tile,
+  TileHeader,
+  TileTitle,
+  TileAction,
+  TileDescription,
+} from "@repo/ui/components/doras-ui/tile";
+import { Label } from "@repo/ui/components/label";
+import { Switch } from "@repo/ui/components/switch";
+import { Toggle } from "@repo/ui/components/toggle";
+
+export default function Preferences() {
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3" title="Public Pages">
+        <Label variant={"subheading"}>General configuration</Label>
+        <div className="bg-card rounded-lg flex flex-col">
+          <Tile className="md:w-full" variant={"transparent"}>
+            <TileHeader className="md:w-full">
+              <TileTitle className="text-sm">
+                Allow actions on closed tasks
+              </TileTitle>
+              <TileDescription className="text-xs leading-normal!">
+                When disabled, users without privileges will not be able to
+                comment on or modify closed tasks.
+              </TileDescription>
+            </TileHeader>
+            <TileAction>
+              <Switch defaultChecked />
+            </TileAction>
+          </Tile>
+        </div>
+      </div>
+      <div className="flex flex-col gap-3" title="Public Pages">
+        <Label variant={"subheading"}>Public page settings</Label>
+        <div className="bg-card rounded-lg flex flex-col">
+          <Tile className="md:w-full" variant={"transparent"}>
+            <TileHeader className="md:w-full">
+              <TileTitle className="text-sm">Enable public page</TileTitle>
+              <TileDescription className="text-xs leading-normal!">
+                Toggles your organizations public page. No external users will
+                be able to find your tasks or contribute.
+              </TileDescription>
+            </TileHeader>
+            <TileAction>
+              <Switch defaultChecked />
+            </TileAction>
+          </Tile>
+          <Tile className="md:w-full" variant={"transparent"}>
+            <TileHeader className="md:w-full">
+              <TileTitle className="text-sm">Public actions</TileTitle>
+              <TileDescription className="text-xs leading-normal!">
+                Allow external users to comment on & create tasks. Enables users
+                to create feature requests, bug reports, etc. Voting remains
+                open.
+              </TileDescription>
+            </TileHeader>
+            <TileAction>
+              <Switch defaultChecked />
+            </TileAction>
+          </Tile>
+          <Tile className="md:w-full" variant={"transparent"}>
+            <TileHeader className="md:w-full">
+              <TileTitle className="text-sm">Blocked users</TileTitle>
+              <TileDescription className="text-xs leading-normal!">
+                Block users from interacting with your organization or its
+                tasks.
+              </TileDescription>
+            </TileHeader>
+            <TileAction>
+              Will contain a sheet with a list of blocked users and options to
+              add or remove users from the list.
+            </TileAction>
+          </Tile>
+        </div>
+      </div>
+    </div>
+  );
+}
--- apps/start/src/routes/(admin)/settings/org/$orgId/index.tsx
+++ apps/start/src/routes/(admin)/settings/org/$orgId/index.tsx
@@ -1,22 +1,29 @@
 import { Label } from "@repo/ui/components/label";
 import { createFileRoute } from "@tanstack/react-router";
-import SettingsOrganizationPage from "@/components/pages/admin/settings/orgId";
+import SettingsOrganizationPage from "@/components/pages/admin/settings/orgId/root";
+import Preferences from "@/components/pages/admin/settings/orgId/root/preferences";
 
 export const Route = createFileRoute("/(admin)/settings/org/$orgId/")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-	return (
-		<div className="max-w-prose mx-auto p-3 md:p-6 w-full flex flex-col gap-9">
-			<div className="flex flex-col">
-				<Label variant="heading" className="text-2xl text-foreground">Organization Settings</Label>
-			</div>
-			<div className="flex flex-col gap-3">
-				<Label variant={"heading"}>General</Label>
+  return (
+    <div className="max-w-prose mx-auto p-3 md:p-6 w-full flex flex-col gap-9">
+      <div className="flex flex-col">
+        <Label variant="heading" className="text-2xl text-foreground">
+          Organization Settings
+        </Label>
+      </div>
+      <div className="flex flex-col gap-3">
+        <Label variant={"heading"}>General</Label>
 
-				<SettingsOrganizationPage />
-			</div>
-		</div>
-	);
+        <SettingsOrganizationPage />
+      </div>
+      <div className="flex flex-col gap-3">
+        <Label variant={"heading"}>Preferences</Label>
+        <Preferences />
+      </div>
+    </div>
+  );
 }
