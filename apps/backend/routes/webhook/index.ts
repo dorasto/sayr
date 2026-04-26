7753afad85d2a078d184877a76b617d639b1c322
trent-001 <trent@doras.to>
2026-04-27 00:50:15 +1000

    more logs on polar webhook

--- apps/backend/routes/webhook/index.ts
+++ apps/backend/routes/webhook/index.ts
@@ -2,11 +2,7 @@ import type { AppEnv } from "@/index";
 import { Hono } from "hono";
 import { PolarWebhookHandler } from "./polar";
 import { GithubWebhookHandler } from "./github";
-import { getEditionCapabilities } from "@repo/edition";
 
 export const webhookRoute = new Hono<AppEnv>();
 webhookRoute.route("/github", GithubWebhookHandler);
-const { polarBillingEnabled } = getEditionCapabilities()
-if (polarBillingEnabled) {
-    webhookRoute.route("/polar", PolarWebhookHandler);
-}
+webhookRoute.route("/polar", PolarWebhookHandler);
